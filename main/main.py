c48c19ca9c9da54cee5291240e5fc05e8f385859
Leon Cruz <leon.cruz.teixeira@gmail.com>
2020-12-28 17:22:26 -0300

    Rota para pegar o like do usuário em um determinado produto

--- main/main.py
+++ main/main.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from dataclasses import dataclass
+
+import requests
+from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import UniqueConstraint
@@ -10,13 +13,23 @@ CORS(app)
 db = SQLAlchemy(app)
 
 
+@dataclass
 class Product(db.Model):
+    id: int
+    title: str
+    image: str
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     title = db.Column(db.String(200))
     image = db.Column(db.String(200))
 
 
+@dataclass
 class ProductUser(db.Model):
+    id: int
+    user_id: int
+    product_id: int
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer)
     product_id = db.Column(db.Integer)
@@ -24,9 +37,15 @@ class ProductUser(db.Model):
     UniqueConstraint("user_id", "product_id", name="user_product_id")
 
 
-@app.route("/")
+@app.route("/api/products")
 def index():
-    return "Hello"
+    return jsonify(Product.query.all())
+
+
+@app.route("/api/products/<int:id>/like", methods=["POST"])
+def like(id):
+    req = requests.get("http://localhost:8000/api/user")
+    return jsonify(req.json())
 
 
 if __name__ == "__main__":
